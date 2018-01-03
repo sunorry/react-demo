6eb7257e270da50ac7584efadc62803b6ece1d18
xiao.geng <xiao.geng@wormpex.com>
2018-01-03 16:54:18 +0800

    整理目录

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Search } from './redux/view';
+import { Search } from './view';
 
 ReactDOM.render(
   <Search />,
--- src/mobx/collections/Container.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { observer } from 'mobx-react'
-import DevTools from 'mobx-react-devtools'
-
-@observer
-export default class SearchContailer extends React.Component {
-
-}

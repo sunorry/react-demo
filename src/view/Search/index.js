de578bb3cde6510599b9c0c16dbf20e29e03cb6e
sunorry <sunorry@gmail.com>
2018-01-12 16:44:11 +0800

    MOD: bugfix

--- src/view/Search/index.js
+++ src/view/Search/index.js
@@ -70,7 +70,7 @@ class Search extends React.Component {
 
   componentWillUnmount() {
     this.unmounted = true;
-    this.unsubscribe();
+    this.unsubscribe && this.unsubscribe();
   }
 
   @SetSafeState()
